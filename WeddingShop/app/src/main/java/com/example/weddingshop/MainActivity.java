5be24a36920de0f0c44cf6d9fb00be50af5a3724
Pigniczki Mercédesz Tamara <h880036@stud.u-szeged.hu>
2022-04-19 16:06:56 +0200

    Bejelentkezés/REgsiztráció

--- WeddingShop/app/src/main/java/com/example/weddingshop/MainActivity.java
+++ WeddingShop/app/src/main/java/com/example/weddingshop/MainActivity.java
@@ -1,14 +1,88 @@
 package com.example.weddingshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView register;
+    private EditText editTextEmail, editTextPassword;
+    private Button signIn;
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
+
+        signIn = (Button) findViewById(R.id.signin);
+        signIn.setOnClickListener(this);
+
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextEmail.setOnClickListener(this);
+
+        editTextPassword = (EditText) findViewById(R.id.password);
+        editTextPassword.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.register:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+            case R.id.signin:
+                userLogin();
+                break;
+        }
+    }
+
+    private void userLogin() {
+
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email hiányzik");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            editTextPassword.setError("Jelszó hiányzik");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    startActivity(new Intent(MainActivity.this, Logout.class));
+                }else{
+                    Toast.makeText(MainActivity.this, "Sikeretelen bejelentkezés", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
---
+++ WeddingShop/app/src/main/java/com/example/weddingshop/User.java
@@ -0,0 +1,13 @@
+package com.example.weddingshop;
+
+public class User {
+
+    public String email;
+
+    public User(String email) {
+        this.email = email;
+    }
+
+    public User() {
+    }
+}
